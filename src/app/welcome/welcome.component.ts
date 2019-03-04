1932874571b7c1ef79ff1ccdb093524fa1f5bd68
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-03-04 23:12:36 +0200

    Design overhaul, welcome page almost complete

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  faCoffee,
+  faAd,
+  faAddressBook,
+  faAirFreshener
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  selector: "app-welcome",
+  templateUrl: "./welcome.component.html",
+  styleUrls: ["./welcome.component.css"]
 })
 export class WelcomeComponent implements OnInit {
+  faCoffee = faCoffee;
+  faAd = faAd;
+  faAddressBook = faAddressBook;
+  faAirFreshener = faAirFreshener;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
