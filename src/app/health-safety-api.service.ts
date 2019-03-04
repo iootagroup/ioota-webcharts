a36bb6e25c8e96a35d372043f02a89c16cf0c5d8
sampoharjula <sampo.harjula@student.lamk.fi>
2019-03-04 17:05:35 +0200

    install fontawesome

--- src/app/health-safety-api.service.ts
+++ src/app/health-safety-api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,4 +18,16 @@ export class HealthSafetyAPIService {
     return this.http.get(environment.API_URL + '/health');
   }
 
+  /**
+   *  Test the API response
+   */
+  getSearch(type: string = '', start: string = '', end: string = '', limit: number = 100, from: number = 0) {
+    let queryString = '';
+
+    if (type != ''){
+      queryString += 'type=' + type;
+    }
+
+    return this.http.get(environment.API_URL + '/api/sensordata?' + queryString);
+  }
 }
